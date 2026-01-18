34813633c0f5038546eb86610ac640d3dce40cfc
Nitai J. Perez <nitaiperez@gmail.com>
2026-01-19 01:38:23 +0200

    refactor: remove deprecated event files and components

--- src/events/todo-list-events.ts
+++
@@ -1,9 +0,0 @@
-import { TodoItem } from "../types/todo";
-
-export type TodoFilter<T> = (todoItem: TodoItem<T>) => boolean;
-
-export interface OpenFileEvent<T> {
-  file: T;
-  line: number;
-  inOtherLeaf: boolean;
-}
